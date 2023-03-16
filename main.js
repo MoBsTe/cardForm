b1408d636be045a1094dabe1cde812fa51f97d74
MoBsTe <valery.lobach@gail.com>
2023-03-16 21:22:33 +0100

    add

--- main.js
+++ main.js
@@ -43,17 +43,8 @@ formCardNum.addEventListener('input', () => {
 
 function cc_format(value) {
     const v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
-    const matches = v.match(/\d{4,16}/g);
-    const match = matches && matches[0] || ''
-    const parts = []
-    for (i = 0, len = match.length; i < len; i += 4) {
-        parts.push(match.substring(i, i + 4))
-    }
-    if (parts.length) {
-        return parts.join(' ')
-    } else {
-        return value
-    }
+    const matches = v.match(/\d{1,4}/g);
+    return matches.join(' ');
 }
 
 // Input Month
